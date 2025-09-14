a1725c4af54f828c990354ac9913412a24f1186b
inginiar <inginiar@gmail.com>
2025-09-14 16:56:36 +0300

    Углубление в Playwright

---
+++ playwright_autorization.py
@@ -0,0 +1,15 @@
+from playwright.sync_api import sync_playwright, expect
+
+
+with sync_playwright() as playwright:
+    chromium = playwright.chromium.launch(headless=False)
+    page = chromium.new_page()
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+    email_input = page.get_by_test_id("login-form-email-input").locator('input').fill("user.name@gmail.com")
+    password_input = page.get_by_test_id("login-form-password-input").locator('input').fill("password")
+    login_button = page.get_by_test_id("login-page-login-button").click()
+    wrong_email_or_password_alert = page.get_by_test_id("login-page-wrong-email-or-password-alert")
+    expect(wrong_email_or_password_alert).to_be_visible()
+    expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
+    page.wait_for_timeout(5000)
+
