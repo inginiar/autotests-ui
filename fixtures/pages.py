ba5d2fde87ae8e618028c4800c9506feeb1665ef
inginiar <inginiar@gmail.com>
2026-01-15 19:27:25 +0300

    7.7 Наращивание тестовой базы и рефакторинг

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -2,11 +2,11 @@ import pytest
 from playwright.sync_api import Page
 
 from fixtures.browsers import chromium_page_with_state
-from pages.courses_list_page import CoursesListPage
-from pages.login_page import LoginPage
-from pages.registration_page import RegistrationPage
-from pages.dashboard_page import DashboardPage
-from pages.create_course_page import CreateCoursePage
+from pages.courses.courses_list_page import CoursesListPage
+from pages.authentication.login_page import LoginPage
+from pages.authentication.registration_page import RegistrationPage
+from pages.dashboard.dashboard_page import DashboardPage
+from pages.courses.create_course_page import CreateCoursePage
 
 @pytest.fixture
 def login_page(chromium_page: Page) -> LoginPage:
--- pages/authentication/registration_page.py
+++ pages/authentication/registration_page.py
@@ -3,13 +3,15 @@ from pages.base_page import BasePage
 from playwright.sync_api import Page
 from elements.button import Button
 from elements.link import Link
+import re
+
 
 class RegistrationPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
         self.registration_form = RegistrationFormComponent(page)
-        self.registration_button = Button(page,"registration-page-registration-button", "Registration")
+        self.registration_button = Button(page, "registration-page-registration-button", "Registration")
         self.login_link = Link(page, "registration-page-login-link", "Login")
 
     def click_registration_button(self):
@@ -17,3 +19,4 @@ class RegistrationPage(BasePage):
 
     def click_login_link(self):
         self.login_link.click()
+        self.check_current_url(re.compile(".*/#/auth/login "))
