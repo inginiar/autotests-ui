24a1e2c16d5756d86d74959cf84ef7d68da60885
inginiar <inginiar@gmail.com>
2026-06-30 18:02:51 +0300

    10.3 Подготовка проекта к запуску на CI

--- tools/allure/environment.py
+++ tools/allure/environment.py
@@ -1,8 +1,12 @@
+import platform
+import sys
 from config import settings
 
 
 def create_allure_environment_file():
-    items = [f'{key}={value}' for key, value in settings.model_dump().items()]
+    os_info = f'os_info={platform.system()}, {platform.release()}'
+    python_version = f'python_version={sys.version}'
+    items = [f'{key}={value}' for key, value in settings.model_dump().items()] + [os_info, python_version]
     properties = '\n'.join(items)
 
     with open(settings.allure_results_dir.joinpath('environment.properties'), 'w+') as file:
